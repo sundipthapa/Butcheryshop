db6337ba4c9b899d382352dd6f49a7a1be400a4e
sundipthapa <lakhanmgr123@gmail.com>
2023-01-09 21:52:46 +0545

    :sparkles: forgot screen design

---
+++ lib/screens/auth/forgotpassword.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ForgetPasswordScreen extends StatefulWidget {
+  const ForgetPasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  _ForgetPasswordScreenState createState() => _ForgetPasswordScreenState();
+}
+
+class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    style: const TextStyle(
+                        fontFamily: 'WorkSansSemiBold',
+                        fontSize: 16.0,
+                        color: Colors.black),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      border: InputBorder.none,
+                      prefixIcon: const Icon(
+                        Icons.email,
+                        color: Colors.black,
+                        size: 22.0,
+                      ),
+                      hintText: 'Email Address',
+                      hintStyle: const TextStyle(
+                          fontFamily: 'WorkSansSemiBold', fontSize: 17.0),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                      side: BorderSide(color: Colors.blue))),
+                          padding: MaterialStateProperty.all<EdgeInsets>(
+                              EdgeInsets.symmetric(vertical: 20)),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          "Reset Password",
+                          style: TextStyle(fontSize: 20),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already know the password? ",
+                        style: TextStyle(color: Colors.grey.shade800),
+                      ),
+                      InkWell(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text(
+                            "Sign In",
+                            style: TextStyle(color: Colors.blue),
+                          ))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
