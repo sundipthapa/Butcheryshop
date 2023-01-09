4e11b4a6916472340f0caacca6db7182eeb78694
sundipthapa <lakhanmgr123@gmail.com>
2023-01-09 18:36:38 +0545

    :sparkles: splash screen design initiated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:solo_flutter_project/screens/auth/login.dart';
 import 'package:solo_flutter_project/screens/auth/register.dart';
+import 'package:solo_flutter_project/screens/auth/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RegisterScreen(),
+      home: const SplashScreen(),
     );
   }
 }
--- lib/screens/auth/splashscreen.dart
+++ lib/screens/auth/splashscreen.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Image.asset("assets/images/splash.gif"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
