2482877e6ff75ae35111ecd1cbf8c7061e60bd37
sundipthapa <lakhanmgr123@gmail.com>
2023-01-09 18:48:49 +0545

    :lipstick: changes on ui of splash screen

--- lib/screens/auth/splashscreen.dart
+++ lib/screens/auth/splashscreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:solo_flutter_project/services/splash_services.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,6 +10,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  SplashServices splashServices = SplashServices();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    splashServices.isLogin(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
